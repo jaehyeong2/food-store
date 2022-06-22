4cef8a90c08b96d485422690338a9c3602703f86
jae <wogud1514@naver.com>
2022-06-22 21:19:12 +0900

    시작

---
+++ src/main/java/jjfactory/foodstore/FoodStoreApplication.java
@@ -0,0 +1,13 @@
+package jjfactory.foodstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jjfactory/foodstore/FoodStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package jjfactory.foodstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
